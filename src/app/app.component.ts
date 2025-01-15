9b09dfd7f043bdeecfac558ce83891cb2e9098de
noname <147138179+RazvanD7@users.noreply.github.com>
2025-01-15 19:37:15 +0200

    Fixed bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
+    
   `,
-  styles: [],
+  styles: []
 })
 export class AppComponent {
   title = 'store';
