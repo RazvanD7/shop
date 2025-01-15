49458767e3fb434695447120348c928b820bcf64
noname <147138179+RazvanD7@users.noreply.github.com>
2025-01-15 22:56:53 +0200

    Finished home page

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { OutgoingMessage } from 'http';
+
 
 @Component({
   selector: 'app-products-header',
   templateUrl: 'products-header.component.html',
 })
 export class ProductsHeaderComponent implements OnInit {
+  @Output() columnsCountChange = new EventEmitter<number>();
   sort = 'desc';
+  itemsShowCount = 12;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -13,5 +18,12 @@ export class ProductsHeaderComponent implements OnInit {
 
   onSortUpdate(newSort: string): void{
     this.sort = newSort;
+  }
+  onItemsUpdated(count: number): void{
+    this.itemsShowCount = count;
+  }
+
+  onColumnsUpdated(colsNum: number): void{
+    this.columnsCountChange.emit(colsNum);
   }
 }
